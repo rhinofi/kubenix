024f5ebd7dea6ae2baea2e1d4b2a4692fa855042
Adrian Gierakowski <agierakowski@gmail.com>
2021-08-05 17:07:24 +0100

    set default k8s version to 1.18

--- modules/k8s.nix
+++ modules/k8s.nix
@@ -202,7 +202,7 @@ in {
     version = mkOption {
       description = "Kubernetes version to use";
       type = types.enum ["1.7" "1.8" "1.9" "1.10" "1.11" "1.12" "1.13" "1.14" "1.15" "1.16" "1.17" "1.18"];
-      default = "1.15";
+      default = "1.18";
     };
 
     namespace = mkOption {
